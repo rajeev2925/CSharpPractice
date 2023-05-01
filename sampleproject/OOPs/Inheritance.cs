3d06b921654ed67cd4c0992fc1051ef1bef07bde
rajeev2925 <rajeevbp2925@gmail.com>
2023-05-01 10:46:42 +0530

    oops practice

---
+++ sampleproject/OOPs/Inheritance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleproject.OOPs
+{
+    public class Inheritance
+    {
+        public void add()
+        {
+            int id = 01;
+            String name = "rajeev";
+            Console.WriteLine(id + "  " + name);
+        }
+    }
+    public class Base_class : Inheritance
+    {
+        public void ex()
+        {
+            String adress = "hassan";
+            Console.WriteLine(adress);
+        }
+    }
+
+    public class Main_Class
+    {
+        public static void main()
+        {
+            Base_class bs=new Base_class();
+            bs.add();
+            bs.ex();
+                    
+        }
+    }
+}
---
+++ sampleproject/OOPs/polymorpizm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleproject.OOPs
+{
+    internal class polymorpizm
+    {
+        public void Add1(int a)
+        {
+            Console.WriteLine(a);
+        }
+        public void Add2(int b,String name)
+        {
+            Console.WriteLine(b + " " + name);
+        }
+        public void Add3(int c,String add)
+        {
+            Console.WriteLine(add + " " + c);
+        }
+    }
+    public class pclass
+    {
+        public static void main(String[] args)
+        {
+            polymorpizm pm=new polymorpizm();   
+            pm.Add1(1);
+            pm.Add2(3, "rajeev");
+            pm.Add3(4, "naga");
+        }
+    }
+
+
+}
